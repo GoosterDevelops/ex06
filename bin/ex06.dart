366ec5b54c93a229d9d092783dbeb0a5101257b7
Francois-Jerome Gosselin <francoisjgosselin@hotmail.com>
2014-11-12 15:44:02 -0500

    Changes to differ from other works

--- bin/ex06.dart
+++ bin/ex06.dart
@@ -4,43 +4,62 @@ void main() {
   
   String theWord = 'Laval';
   String notPalindrome = 'Whale';
+  print('\n--------------');
   print('Question 1:');
+  print('--------------\n');
   print("${isPalindrome(theWord)}");
+  
   print("${isPalindrome(notPalindrome)} \n");
   
 //Question 2
   int year = 1993;
   int month = 06;
   int day = 10;
+  print('\n--------------');
   print('Question 2:');
+  print('--------------\n');
   print("${dayCount(year, month, day)} \n");
   
 //Question3:
+  print('\n--------------');
   print('Question 3:');
-  print("98% = ${toLetter(98)}");
-  print("82% = ${toLetter(82)}");
-  print("74% = ${toLetter(74)}");
-  print("57% = ${toLetter(57)}");
-  print("35% = ${toLetter(35)} \n");  
+  print('--------------\n');
+  print('Returning a letter grade to convert % to standard:');
+  print("${toLetter(98)}");
+  print("${toLetter(82)}");
+  print("${toLetter(74)}");
+  print("${toLetter(57)}");
+  print("${toLetter(35)} \n");  
   
 //Question 4:
   List names = ['Francois', 'Dave', 'Simon', 'Jason', 'Pierre-Olivier', 'Jean-Pascale', 'Sharonne', 'Julie', 'Suzanne'];
+  print('\n--------------');
   print('Question 4:');
+  print('--------------\n');
   print("Original list: $names");
   print("Processed list: ${createList(names)} \n");
       
 //Question 5:
 
-print ('Question 5');
-var players={'PK Subban':'Canadiens', 'Lars Eller':'Canadiens', 'Sidney Crosby':'Pitsburg', 
-             'Evgeni Malkin':'Pitsburg', 'Chris Letang':'Pitsburg', 'Marc-Andre Fleury':'Pitsburg','Derek Stepan':'Rangers', 'Jonathan Quick':'Rangers', 'Martin St-louis':'Rangers', 'Tailor Hall':'Oilers', 'David Perron':'Oilers'};
-var clubs=['Canadiens', 'Pitsburg', 'Rangers', 'Oilers'];
+  print('\n--------------');
+  print('Question 5:');
+  print('--------------\n');
 
-var new_list=new Map();
-print('List of players with their clubs: \n${players}');
-print('List of clubs:\n${clubs}');
-new_list=playersClubs(players,clubs);
-print('List of clubs ordered with their players');
-new_list.forEach((x,y){ print('$x $y'); });
-    
+
+  List nameOfPlayers = [['Desharnais', 'Prust', 'Price'], ['St-Louis', 'Lundvisqt', 'Brassard'], ['Miller', 'Sedin', 'Burrows'], ['Crosby', 'Malkin', 'Fleury']];
+  List nameOfClubs = ['Montreal Canadiens', 'New-York Rangers', 'Vancouver Canucks', 'Pittsburg Penguins'];
+  print('Players => $nameOfPlayers');
+  print('Clubs => $nameOfClubs');
+  print('Players in respective clubs => ${playersClubs(nameOfPlayers, nameOfClubs)}');
+  print("");
+
+
+void printList(List l) {
+  
+  for (int i = 0; i < l.length; i++) {
+    print(l.elementAt(i).toString());
+  }
+  
+  return;
+}
 }
--- lib/functions.dart
+++ lib/functions.dart
@@ -1,57 +1,57 @@
-
 library define_functions_ex06;
 
 //question 1 function
 String isPalindrome(String theWord) {
+  
   String toLower = theWord.toLowerCase();
   for (var i = 0; i < toLower.length / 2; i++) {
-    if (toLower[i] != toLower[toLower.length - i - 1]) {
-      return "$theWord is not a palindrome.";
-    }
+      if (toLower[i] != toLower[toLower.length - i - 1]) {
+        return "$theWord is not a palindrome.";
+      }
   }
-  return "$theWord is a palindrome.";
+ return "$theWord is a palindrome.";
 }
 
 //question 2 function
+// Function based on time based Dart methods... Easier to calculate datetime objects.
 String dayCount(int year, int month, int day){
 DateTime dateNow = new DateTime.now();
 DateTime dateCustom = new DateTime.utc(year, month, day);
 Duration difference = dateNow.difference(dateCustom);
 int difference2 = difference.inDays;
+
 return "There is $difference2 days between $dateCustom and $dateNow";
 }
 
 //question 3 function
 String toLetter(int grade) {
-List letter = ["A+", "A", "A-", "B+", "B", "B-", "C+", "C",
-"C-", "D+", "D", "E"];
 if(grade >= 96) {
-return letter[0];
+return grade.toString() + '% corresponds to A+';
 } else if (96 > grade && grade > 90) {
-return letter[1];
+return grade.toString() + '% corresponds to A';
 } else if (90 >= grade && grade > 87) {
-return letter[2];
+return grade.toString() + '% corresponds to A-';
 } else if (87 >= grade && grade > 84) {
-return letter[3];
+return grade.toString() + '% corresponds to B+';
 } else if (84 >= grade && grade > 80) {
-return letter[4];
+return grade.toString() + '% corresponds to B';
 } else if (80 >= grade && grade > 77) {
-return letter[5];
+return grade.toString() + '% corresponds to B-';
 } else if (77 >= grade && grade > 73) {
-return letter[6];
+return grade.toString() + '% corresponds to C+';
 } else if (73 >= grade && grade > 68) {
-return letter[7];
+return grade.toString() + '% corresponds to C';
 } else if (68 >= grade && grade > 65) {
-return letter[8];
+return grade.toString() + '% corresponds to C-';
 } else if (65 >= grade && grade > 60) {
-return letter[9];
+return grade.toString() + '% corresponds to D+';
 } else if (60 >= grade && grade > 50) {
-return letter[10];
+return grade.toString() + '% corresponds to D';
 } else if (50 >= grade) {
-return letter[11];
+return grade.toString() + '% corresponds to F. You fail!!!';
 }
 else{
-return "erreur"; }
+return "error"; }
 }
 
 //Question 4 function
@@ -59,39 +59,36 @@ List createList(List names) {
  List shorter8Letters = new List();
  List with8Letters = new List();
  List longer8Letters = new List();
- List allNames = new List();
- 
-for (int i = 0; i < names.length; i++) {
-if (names[i].length > 8) {
-  longer8Letters.add(names[i]);
-} else if (names[i].length == 8) {
-  with8Letters.add(names[i]);
-} else if (names[i].length < 8) {
-  shorter8Letters.add(names[i]);
-}
-}
-allNames.add(shorter8Letters);
-allNames.add(with8Letters);
-allNames.add(longer8Letters);
-return allNames;
-
-}
-
-//question 5 function
-
-Map playersClubs (var players, var clubs){
-  var playersWithClubs = new Map();
-  var list2 = new List();
-  clubs.sort((a,b){ return a.compareTo(b);});
-  for (var i=0;i<clubs.length;i++){
-    players.forEach(
-    (x,y){
-    if(y==clubs[i])
-    list2.add(x);
+ List allnames = new List();
+ var nameLength = names.length;
+  for (int i = 0; i < nameLength; i++) {
+    if (names[i].length > 8) {
+      longer8Letters.add(names[i]);
+    } 
+    
+    else if (names[i].length == 8) {
+      with8Letters.add(names[i]);
+    } 
+    
+    else if (names[i].length < 8) {
+      shorter8Letters.add(names[i]);
     }
-  );
-  playersWithClubs[ clubs[i] ] = new List.from(list2);
-  list2.clear();
+  }
+allnames.add(longer8Letters);
+allnames.add(with8Letters);
+allnames.add(shorter8Letters);
+return allnames;
 }
-return playersWithClubs;
+
+
+//Question 5 function
+//Help taken from http://stackoverflow.com/questions/18244545/dart-how-to-sort-maps-keys
+Map playersClubs(List nameOfPlayers, List nameOfClubs) {
+  var map = new Map();
+  var clubCount = nameOfClubs.length;
+  for (var i = 0; i < clubCount; i++) {
+    map [nameOfClubs[i]] = nameOfPlayers[i];
+  }
+  return map;
 }
+
